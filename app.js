8603b65e792980426d18f7da74c35a34b1abc1b6
andrey <andrey@lionhearth.com>
2012-01-03 19:40:45 +0100

    Dependencies was defined and project initial configuration done

--- app.js
+++ app.js
@@ -6,13 +6,22 @@
 var express = require('express')
   , routes = require('./routes')
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(
+	express.cookieParser(),
+	express.session({ secret: 'GFTYUrtyfygfT^&**uyhgiugiuyg67fyt' })
+	);
+	
+app.register('html', require('ejs'));
+// DB
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/consuming');
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
