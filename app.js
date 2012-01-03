fa26e8dabd9b37a62a54d939bcb128cb67f96103
andrey <andrey@lionhearth.com>
2012-01-03 22:08:11 +0100

    google auth was added

--- app.js
+++ app.js
@@ -5,6 +5,38 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , everyauth = require('everyauth');
+
+everyauth.debug = true;
+
+var usersById = {};
+var nextUserId = 0;
+var usersByGoogleId = {};
+
+function addUser (source, sourceUser) {
+  var user;
+  if (arguments.length === 1) { // password-based
+    user = sourceUser = source;
+    user.id = ++nextUserId;
+    return usersById[nextUserId] = user;
+  } else { // non-password-based
+    user = usersById[++nextUserId] = {id: nextUserId};
+    user[source] = sourceUser;
+  }
+  return user;
+}
+
+everyauth.google
+  .appId('198255835595.apps.googleusercontent.com')
+  .appSecret('gpAeYLUaZWQxklj3oe4sUPwC')
+  .scope('https://www.google.com/m8/feeds/')
+  .findOrCreateUser( function (sess, accessToken, extra, googleUser) {
+    googleUser.refreshToken = extra.refresh_token;
+    googleUser.expiresIn = extra.expires_in;
+    return usersByGoogleId[googleUser.id] || (usersByGoogleId[googleUser.id] = addUser('google', googleUser));
+  })
+  .redirectPath('/');	
+
 
 var app = module.exports = express.createServer(
 	express.cookieParser(),
@@ -12,12 +44,6 @@ var app = module.exports = express.createServer(
 	);
 	
 app.register('html', require('ejs'));
-// DB
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/consuming');
-
-// Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -26,7 +52,19 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(everyauth.middleware());
 });
+
+
+
+// DB
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/consuming');
+
+// Configuration
+
+
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
@@ -37,6 +75,11 @@ app.configure('production', function(){
 });
 
 // Routes
+
+
+
+
+everyauth.helpExpress(app);
 
 app.get('/', routes.index);
 
