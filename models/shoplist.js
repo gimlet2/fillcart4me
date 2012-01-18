0eb75d0c604ba92b21c051f87ff51b0a2f2899ca
andrey <andrey@lionhearth.com>
2012-01-18 13:41:26 +0100

    ShopList model refactoring done.

--- models/shoplist.js
+++ models/shoplist.js
@@ -1,157 +1,122 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema
 var ObjectId = Schema.ObjectId;
-var fs = require('fs');
 
 var Item = require('./item.js');
 var ItemModel = mongoose.model('Item', Item);
 
 var ShopList = exports = module.exports = new Schema({
-	_id		 : ObjectId
-  , name     : String
-  , owner	 : String
-  , coOwners : [String]
-  , items	 : [Item]
+    _id         : ObjectId
+    , name      : String
+    , owner     : String
+    , coOwners  : [String]
+    , items     : [Item]
 });
 
-var ShopListModel = exports.model  = mongoose.model('ShopList', ShopList);
+var ShopListModel = exports.model = mongoose.model('ShopList', ShopList);
 
-
-
-exports.get = function(id, ownerId, fn){
-  ShopListModel.find({$or:[{owner: ownerId},{ coOwners: { $in: [ownerId] }}], _id: id}, function(err, doc) {
-		checkError(err, doc, function() {fn(doc[0]);});
-	});
-};
-
-exports.getAll = function(id, fn) {
-	var query = ShopListModel.find({ $or:[{owner: id},{ coOwners: { $in: [id] }}] },['_id', 'name', 'owner']);
-	query.exec(function(err, doc) {
-		checkError(err, doc, fn);
-	});
-	//ShopListModel.find({owner: id}, 
+checkError = function(error, data, callBack) {
+    callBack(data);
+    if (error != null) {
+        console.error(error);
+    }
 }
 
-checkError = function(err, doc, fn) {
-		if(err == null) {
-			fn(doc);
-		} else {
-			fn(null);
-  			console.log(err);
-		}
+exports.getShopList = function(userId, shopListId, callBack, failBack) {
+    ShopListModel.findOne({
+                $or:[
+                    {owner: userId},
+                    { coOwners: { $in: [userId] }}
+                ],
+                _id: shopListId},
+            function(error, result) {
+                isShopListFound(error, result, function() {
+                    callBack(result);
+                }, failBack);
+            });
 }
 
-exports.create = function(everyauth, name, fn) {
-	var shopList = new ShopListModel();
-		shopList.name = name;
-		shopList.owner = everyauth.google.user.id;
-		shopList.save(function(err) {
-			checkError(err, null, function() {
-				exports.getAll(everyauth.google.user.id, fn);
-			});
-		});
-}
-
-exports.delete = function(everyauth, id, fn) {
-	var query = ShopListModel.find({});
-	query.where('_id', id).where('owner', everyauth.google.user.id).remove(function() {
-		exports.getAll(everyauth.google.user.id, fn);
-	});
-
-}
-
-exports.addItem = function(everyauth, id, itemName, fn) {
-
-	exports.get(id, everyauth.google.user.id, function(shopList) {
-		if(shopList == null) {
-			console.log('shopList not found');
-		} else {
-			var item = new ItemModel();
-			item.name = itemName;
-			item.isBought = false;
-			shopList.items.push(item);
-			shopList.save(function(err) {
-				checkError(err, null, function() {
-					fn(shopList);
-				});
-			});
-
-		}
-	});
-
+exports.getShopListsForUser = function(userId, callBack) {
+    ShopListModel.find({
+                $or:[
+                    {owner: userId},
+                    { coOwners: { $in: [userId] }}
+                ] }
+            , ['_id', 'name', 'owner'])
+            .exec(function(error, result) {
+                isShopListFound(error, result, callBack);
+            });
 }
 
 
-exports.deleteItem = function(everyauth, listId, id, fn) {
-	console.log(listId);
-	exports.get(listId, everyauth.google.user.id, function(shopList) {
-		if(shopList == null) {
-			console.log('shopList not found');
-		} else {
-			shopList.items.id(id).remove();
-			shopList.save(function(err) {
-				checkError(err, null, function() {
-					fn(shopList);
-				});
-			});
-
-		}
-	});
-
+exports.createShopList = function(userId, shopListName, callBack) {
+    var shopList = new ShopListModel();
+    shopList.name = shopListName;
+    shopList.owner = userId;
+    saveShopList(shopList, function() {
+        exports.getShopListsForUser(userId, callBack);
+    });
 }
 
-exports.buyItem = function(everyauth, listId, id, fn) {
-	console.log(listId);
-	exports.get(listId, everyauth.google.user.id, function(shopList) {
-		if(shopList == null) {
-			console.log('shopList not found');
-		} else {
-			shopList.items.id(id).isBought = true;
-			shopList.save(function(err) {
-				checkError(err, null, function() {
-					fn(shopList);
-				});
-			});
-
-		}
-	});
-
+exports.deleteShopList = function(userId, shopListId, callBack) {
+    ShopListModel.find({})
+            .where('_id', shopListId)
+            .where('owner', userId)
+            .remove(function() {
+                exports.getShopListsForUser(userId, callBack);
+            });
 }
 
-exports.addCoOwner = function(everyauth, id, name, fn) {
+exports.addItemToShopList = function(userId, shopListId, itemName, callBack) {
+    exports.getShopList(userId, shopListId, function(shopList) {
+        var item = new ItemModel();
+        item.name = itemName;
+        item.isBought = false;
+        shopList.items.push(item);
+        saveShopList(shopList, callBack);
+    });
+}
 
-	exports.get(id, everyauth.google.user.id, function(shopList) {
-		if(shopList == null) {
-			console.log('shopList not found');
-		} else {
-			shopList.coOwners.push(name);
-			shopList.save(function(err) {
-				checkError(err, null, function() {
-					fn(shopList);
-				});
-			});
+exports.deleteItemFromShopList = function(userId, shopListId, itemId, callBack) {
+    exports.getShopList(userId, shopListId, function(shopList) {
+        shopList.items.id(itemId).remove();
+        saveShopList(shopList, callBack);
+    });
+}
 
-		}
-	});
+exports.buyItemFromShopList = function(userId, shopListId, itemId, callBack) {
+    exports.getShopList(userId, shopListId, function(shopList) {
+        shopList.items.id(itemId).isBought = true;
+        saveShopList(shopList, callBack);
+    });
+}
 
+exports.addCoOwnerForShopList = function(userId, shopListId, coOwnerId, callBack) {
+    exports.getShopList(userId, shopListId, function(shopList) {
+        shopList.coOwners.push(coOwnerId);
+        saveShopList(shopList, callBack);
+    });
 }
 
 
-exports.deleteCoOwner = function(everyauth, listId, id, fn) {
-	console.log(listId);
-	exports.get(listId, everyauth.google.user.id, function(shopList) {
-		if(shopList == null) {
-			console.log('shopList not found');
-		} else {
-			shopList.coOwners.remove(id);
-			shopList.save(function(err) {
-				checkError(err, null, function() {
-					fn(shopList);
-				});
-			});
-
-		}
-	});
-
+exports.deleteCoOwnerFromShopList = function(userId, shopListId, coOwnerId, callBack) {
+    exports.getShopList(userId, shopListId, function(shopList) {
+        shopList.coOwners.remove(coOwnerId);
+        saveShopList(shopList, callBack);
+    });
 }
 
+saveShopList = function(shopList, callBack) {
+    shopList.save(function() {
+        callBack(shopList);
+    });
+}
+
+isShopListFound = function(shopList, callBack, failBack) {
+    if (shopList == null) {
+        console.error('shopList not found');
+        failBack();
+    } else {
+        callBack();
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,58 @@
-
 /*
  * GET home page.
  */
 var shopList = require('../models/shoplist.js');
 
 
-exports.index = function(req, res){
-	if(req.session.auth && req.session.auth.loggedIn) {
-  		req.session.userId = req.session.auth.google.user.id;
-  		
-		shopList.getAll(req.session.userId, function() {
-  			res.render('index.html', { title: 'Main', layout: 'main'  });
-  		});
+exports.index = function(req, res) {
+    if (req.session.auth && req.session.auth.loggedIn) {
+        req.session.userId = req.session.auth.google.user.id;
 
-	} else {
-		res.render('index.html', { title: 'Main', layout: 'main' });
-  	}
+        shopList.getAll(req.session.userId, function() {
+            res.render('index.html', { title: 'Main', layout: 'main'  });
+        });
+
+    } else {
+        res.render('index.html', { title: 'Main', layout: 'main' });
+    }
 };
 
-exports.about = function(req, res){
-  res.render('about.html', { title: 'About', layout: 'main' })
+exports.about = function(req, res) {
+    res.render('about.html', { title: 'About', layout: 'main' })
 };
 
 exports.addShopList = function(everyauth, shopListName, now) {
-		shopList.create(everyauth, shopListName, now.displayPollData);
+    shopList.createShopList(everyauth.google.user.id, shopListName, now.displayPollData);
 }
 
 exports.deleteShopList = function(everyauth, id, now) {
-		shopList.delete(everyauth, id, now.displayPollData);
+    shopList.deleteShopList(everyauth.google.user.id, id, now.displayPollData);
 }
 
 exports.getShopLists = function(everyauth, now) {
-		shopList.getAll(everyauth.google.user.id, now.displayPollData);
+    shopList.getShopListsForUser(everyauth.google.user.id, now.displayPollData);
 }
 
 exports.getShopList = function(everyauth, now, id) {
-		shopList.get(id, everyauth.google.user.id, now.displayPollItemsData);
+    shopList.getShopList(everyauth.google.user.id, id, now.displayPollItemsData, now.displayPollData);
 }
 
 exports.addItem = function(everyauth, id, itemName, now) {
-		shopList.addItem(everyauth, id, itemName, now.displayPollItemsData);
+    shopList.addItemToShopList(everyauth.google.user.id, id, itemName, now.displayPollItemsData);
 }
 
 exports.deleteItem = function(everyauth, listId, id, now) {
-		shopList.deleteItem(everyauth,listId, id, now.displayPollItemsData);
+    shopList.deleteItemFromShopList(everyauth.google.user.id, listId, id, now.displayPollItemsData);
 }
 
 exports.buyItem = function(everyauth, listId, id, now) {
-		shopList.buyItem(everyauth,listId, id, now.displayPollItemsData);
+    shopList.buyItemFromShopList(everyauth.google.user.id, listId, id, now.displayPollItemsData);
 }
 
 exports.addCoOwner = function(everyauth, id, itemName, now) {
-		shopList.addCoOwner(everyauth, id, itemName, now.displayPollItemsData);
+    shopList.addCoOwnerForShopList(everyauth.google.user.id, id, itemName, now.displayPollItemsData);
 }
 
 exports.deleteCoOwner = function(everyauth, listId, id, now) {
-		shopList.deleteCoOwner(everyauth,listId, id, now.displayPollItemsData);
+    shopList.deleteCoOwnerFromShopList(everyauth.google.user.id, listId, id, now.displayPollItemsData);
 }
