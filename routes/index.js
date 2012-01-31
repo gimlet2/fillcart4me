79f9db786248ca089584ac788d56e113836587a1
andrey <andrey@lionhearth.com>
2012-01-31 15:47:00 +0100

    ShopLists preload done

--- routes/index.js
+++ routes/index.js
@@ -3,60 +3,62 @@
  */
 var shopList = require('../models/shoplist.js');
 var user = require('../models/user.js');
+var notify = require('../notify.js');
 
-exports.authUser = function(user) {
+exports.authUser = function (user) {
 
 }
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     if (req.session.auth && req.session.auth.loggedIn) {
         req.session.userId = req.session.auth.google.user.id;
 
-        shopList.getShopListsForUser(req.session.userId, function() {
-            res.render('index.html', { title: 'Main', layout: 'main'  });
+        shopList.getShopListsForUser(req.session.userId, function (result) {
+            res.render('index.html', { title:'Main', layout:'main', preloadShopLists:result });
         });
 
     } else {
-        res.render('index.html', { title: 'Main', layout: 'main' });
+        res.render('index.html', { title:'Main', layout:'main' });
     }
 };
 
-exports.about = function(req, res) {
-    res.render('about.html', { title: 'About', layout: 'main' })
+exports.about = function (req, res) {
+    notify.sendNotifyEmail(req.session.auth.google.user.id);
+    res.render('about.html', { title:'About', layout:'main' })
 };
 
-exports.addShopList = function(everyauth, now, shopListName) {
+exports.addShopList = function (everyauth, now, shopListName) {
     shopList.createShopList(everyauth.google.user.id, shopListName, now.displayPollData);
 }
 
-exports.deleteShopList = function(everyauth, now, shopListId) {
+exports.deleteShopList = function (everyauth, now, shopListId) {
     shopList.deleteShopList(everyauth.google.user.id, shopListId, now.displayPollData);
 }
 
-exports.getShopLists = function(everyauth, now) {
+exports.getShopLists = function (everyauth, now) {
     shopList.getShopListsForUser(everyauth.google.user.id, now.displayPollData);
 }
 
-exports.getShopList = function(everyauth, now, shopListId) {
+exports.getShopList = function (everyauth, now, shopListId) {
     shopList.getShopList(everyauth.google.user.id, shopListId, now.displayPollItemsData, now.displayPollData);
 }
 
-exports.addItem = function(everyauth, now, shopListId, itemName) {
+exports.addItem = function (everyauth, now, shopListId, itemName) {
     shopList.addItemToShopList(everyauth.google.user.id, shopListId, itemName, now.displayPollItemsData);
 }
 
-exports.deleteItem = function(everyauth, now, shopListId, itemId) {
+exports.deleteItem = function (everyauth, now, shopListId, itemId) {
     shopList.deleteItemFromShopList(everyauth.google.user.id, shopListId, itemId, now.displayPollItemsData);
 }
 
-exports.buyItem = function(everyauth, now, shopListId, itemId) {
+exports.buyItem = function (everyauth, now, shopListId, itemId) {
     shopList.buyItemFromShopList(everyauth.google.user.id, shopListId, itemId, now.displayPollItemsData);
 }
 
-exports.addCoOwner = function(everyauth, now, shopListId, coOwnerId) {
+exports.addCoOwner = function (everyauth, now, shopListId, coOwnerId) {
     shopList.addCoOwnerForShopList(everyauth.google.user.id, shopListId, coOwnerId, now.displayPollData);
 }
 
-exports.deleteCoOwner = function(everyauth, now, shopListId, coOwnerId) {
+exports.deleteCoOwner = function (everyauth, now, shopListId, coOwnerId) {
     shopList.deleteCoOwnerFromShopList(everyauth.google.user.id, shopListId, coOwnerId, now.displayPollData);
 }
